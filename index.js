864504c4556104370958d3e14b9f03e48b478956
Jun Ki Kwon <pkwon35@gmail.com>
2022-02-16 02:02:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ mongoose.connect(config.mongoURI)
 app.get('/',(req,res)=> res.send('Hello World!~ '))
 
 
-app.post('/api/users//register',(req, res) => {
+app.post('/api/users/register',(req, res) => {
 
     //회원 가입 할때 필요한
     // 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다
