f3e7a07347b22aa1d6c0d05cb07bb61616b6663e
Jun Ki Kwon <pkwon35@gmail.com>
2022-02-16 01:58:37 +0900

    Auth 기능 완료

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const config = require("./config/key")
+const {auth} = require('./middleware/auth')
 const {User} = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -25,7 +26,7 @@ mongoose.connect(config.mongoURI)
 app.get('/',(req,res)=> res.send('Hello World!~ '))
 
 
-app.post('/register',(req, res) => {
+app.post('/api/users//register',(req, res) => {
 
     //회원 가입 할때 필요한
     // 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다
@@ -42,7 +43,7 @@ app.post('/register',(req, res) => {
 
 
 
-app.post('/login',(req,res)=>{
+app.post('/api/users/login',(req,res)=>{
     //요청된 이메일을 데이터베이스에 있는지 찾는다
     User.findOne({email:req.body.email}, (err,user) => {
         
@@ -70,4 +71,23 @@ app.post('/login',(req,res)=>{
 })
 
 
+
+app.get('/api/users/auth',auth,(req,res)=>{
+    // 여기까지 미들웨어를 통화해 왔다는  Authentication 이 True
+    req.status(200).json({
+        _id:req.user._id,
+        isAdmin:req.user.role === 0? false : true,
+        isAuth:true,
+        email:req.user.email,
+        name:req.user.name,
+        lastname:req.user.lastname,
+        role:req.user.role,
+        image:req.user.image
+    })
+})
+
+
+
 app.listen(port,()=>console.log(`Example app listening on port ${port}!`));
+
+
