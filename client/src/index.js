d39e1d8d32b9e9e8c02beb8b5fe9470034376392
Jun Ki Kwon <pkwon35@gmail.com>
2022-02-18 12:15:48 +0900

    redux 연결 완료 -> react 페이지 시작

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
-
+import {Provider} from 'react-redux';
 import 'antd/dist/antd.css';
-ReactDOM.render(
 
-    <App />,
+import {applyMiddleware,createStore} from 'redux';
+import promiseMiddleware from 'redux-promise';
+
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers'
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
+
+
+ReactDOM.render(
+  <Provider 
+      store={createStoreWithMiddleware(Reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )}
+  
+  >
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
